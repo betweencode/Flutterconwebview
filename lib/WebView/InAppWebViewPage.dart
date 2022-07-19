cc59b24262727f4839a6f59f3ca7e54fe429c253
samv <mariscal@gmail.com>
2022-07-18 21:23:36 -0500

    cambiso para webview

---
+++ lib/WebView/InAppWebViewPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class InAppWEbViewPage extends StatefulWidget {
+  String urlWebview;
+  InAppWEbViewPage({Key? key,required this.urlWebview}) : super(key: key);
+
+  @override
+  State<InAppWEbViewPage> createState() => _InAppWEbViewPageState();
+}
+
+class _InAppWEbViewPageState extends State<InAppWEbViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InAppWebView(initialUrlRequest:URLRequest(url: Uri.parse(widget.urlWebview)),),
+    );
+  }
+}
